792ac24e108dd8185bd020b7c0db88707b86c06d
mugambi645 <mugambi645@example.com>
2026-07-06 18:08:37 +0300

    count total num of words

--- projects/bootbot/main.py
+++ projects/bootbot/main.py
@@ -2,12 +2,15 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 def main():
     # Relative path to your frankenstein file
     book_path = "books/frankenstein.txt" 
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()
