eed78e61cfe72656246570bad9a5a4eee185212d
mugambi645 <mugambi645@example.com>
2026-07-07 06:21:27 +0300

    minimal go server

--- projects/go-microservices/main.go
+++ projects/go-microservices/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"net/http"
 	"log"
+	"io/ioutil"
+	"fmt"
 )
 func main() {
 
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(rw, "Error reading body", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Data %s", d)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.HandleFunc("/goodbye", func(_ http.ResponseWriter, _ *http.Request) {
+		log.Println("Goodbye World")
 	})
 	http.ListenAndServe(":9090", nil)
 
